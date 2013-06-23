43f1a01bab4e96d49c14953a23ff4c2a6cec98a0
夏学智 <xxz291917@163.com>
2013-06-23 18:40:45 +0800

    问答首页阶段性提交。

--- application/logs/log-2013-05-24.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-05-24 07:52:15 --> 404 Page Not Found --> main/admin_index
-ERROR - 2013-05-24 07:52:17 --> 404 Page Not Found --> admin/index.php
-ERROR - 2013-05-24 07:52:17 --> 404 Page Not Found --> admin/index.php
-ERROR - 2013-05-24 07:52:18 --> 404 Page Not Found --> admin/index.php
-ERROR - 2013-05-24 07:52:19 --> 404 Page Not Found --> admin/index.php
--- application/logs/log-2013-06-22.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-06-22 15:57:58 --> Query error: Unknown column 'special' in 'where clause'
-ERROR - 2013-06-22 15:57:58 --> Severity: Notice  --> Undefined index: position D:\xampp\htdocs\www\mycode\application\views\topic_show.php 104
-ERROR - 2013-06-22 15:57:59 --> Severity: Notice  --> Undefined index: position D:\xampp\htdocs\www\mycode\application\views\topic_show.php 105
