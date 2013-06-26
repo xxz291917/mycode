4081f41140ed102534c30444e97d71d4f25cfda5
liulianwen <liulianwen@CY5839.cyou-inc.com>
2013-06-26 14:49:12 +0800

    lianwen提交。

--- application/controllers/admin/credits.php
+++ application/controllers/admin/credits.php
@@ -7,7 +7,7 @@ class Credits extends Admin_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model(array('config_model'));
+        $this->load->model(array('credit_name_model'));
     }
 
     /**
@@ -15,7 +15,7 @@ class Credits extends Admin_Controller {
      */
     public function index() {
 
-        $credits = $this->config_model->get_credits_list();
+        $credits = $this->credit_name_model->get_credits_list();
         $var['credits'] = $credits;
         $var['count'] = count($credits);
         $this->view('credits_config', $var);
@@ -26,28 +26,32 @@ class Credits extends Admin_Controller {
      */
     public function update() {
         $str_value = $this->input->post();
+       // print_r($str_value);
         for ($i = 0; $i < 8; $i++) {
             $cx = "credit_x_" . $i;
-            if (!empty($str_value[$cx])) {
-                $str_value[$cx] = 0;
+            if (empty($str_value[$cx])) {
+                $str_value[$cx] =0;
+         
             } else {
                 $str_value[$cx] = 1;
+            
             }
             $vn = "view_name_" . $i;
             $ic = "icon" . $i;
             $un = "unit" . $i;
             $j = $i + 1;
-            $where = "extcredits" . $j;
+            $file="extcredits".$j;
             $data = array(
                 'status' => $str_value[$cx],
                 'view_name' => $str_value[$vn],
                 'icon' => $str_value[$ic],
                 'unit' => $str_value[$un],
             );
-            $this->config_model->update_credit($data, $where);
+            $this->credit_name_model->update_credit($data, $file);
+         }
             echo "<script type=text/javascript>alert('设置成功');history.back();</script>";
             die;
-        }
+      
     }
 
 }
